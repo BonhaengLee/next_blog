1c52f23b1feb9168c8083cbc114d580b2d088f81
BonhaengLee <59217352+BonhaengLee@users.noreply.github.com>
2021-03-01 02:33:38 +0900

    210301 02.jpg

---
+++ pages/write/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Editor from "../../components/write/Editor";
+import Responsive from "../../components/common/Responsive";
+
+const write = () => {
+    return (
+        <Responsive>
+            <Editor />
+        </Responsive>
+    );
+};
+
+export default write;
